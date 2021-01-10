3291ef8149bce925b90a78ef4e443674baf04928
svsannidhay <sannidhayvashalsv@gmail.com>
2021-01-10 18:21:28 +0530

    .DS_Store banished!

--- MATHEMATICS/Basic Number Theory/Modular arithmetic.cpp
+++ MATHEMATICS/Basic Number Theory/Modular arithmetic.cpp
@@ -35,7 +35,7 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////BINARY EXPONENTIATION/////////////////////////////////////////
-ll binaryexponential(ll base, ll exp, ll mod){
+ll binaryExponentiation(ll base, ll exp, ll mod){
     ll value = 1;
     while(exp>0){
         if(exp%2==1)
@@ -45,13 +45,13 @@ ll binaryexponential(ll base, ll exp, ll mod){
     }
     return value;
 }
-
+//Time complexity: O(log N)
 /////////////////////////////////////FAST EXPONENTIATION///////////////////////////////////////////
-ll fastexponential(ll base,ll exp,ll mod){
+ll fastExponentiation(ll base,ll exp,ll mod){
     if(exp==0)  return 1;
     else if(exp==1) return base%mod;
     else{
-        ll r = fastexponential(base,exp/2,mod);
+        ll r = fastExponentiation(base,exp/2,mod);
         if(exp%2==0){
             return ( (r%mod) * (r%mod) )%mod;
         }else{
@@ -59,8 +59,54 @@ ll fastexponential(ll base,ll exp,ll mod){
         }
     }
 }
+//Time complexity: O(log N)
+/////////////////////////////////////EUCLID's ALGORITHM/////////////////////////////////////////////
+ll GCD(ll a,ll b){
+    if(b==0) return a;
+    return GCD(b,a%b);
+}
+//The time complexity is O(log(max(A, B))).
+//////////////////////////////EXTENDED EUCLID's ALGORITHM////////////////////////////////////////////
+void extendedEuclid(ll a,ll b,ll &d,ll &x,ll &y) {
+    if(b == 0) {
+        d = a;
+        x = 1;
+        y = 0;
+    }
+    else {
+        extendedEuclid(b, a%b,d,x,y);
+        ll temp = x;
+        x = y;
+        y = temp - (a/b)*y;
+    }
+}
+//The time complexity of the extended Euclidean algorithm is O(log(max(A, B))).
+//////////////////////////////MODULAR MULTIPLICATIVE INVERSE/////////////////////////////////////////
+//NAIVE APPROACH
+ll modInverse(ll a,ll m){
+    a=a%m;
+    for(ll b=1;b<m;b++){
+        ll val = (a*b)%m;
+        if(val == 1) return b;   
+    } 
+    return -1; //MMI doesn't exists.        
+}
+//Time complexity O(M)
+//USING EXTENDED EUCLID'S
+ll modInverseEEA(ll a,ll m,ll &d,ll &x,ll &y){
+    extendedEuclid(a, m,d,x,y);
+    return (x % m + m) % m; //x may be negative
+}
+//WHEN M is PRIME USE : - BINARY EXPONENTIATION
+// Fermat's Little Theorem. (A^(M-1)) = 1 mod M;
+// Multiplying b/s by A^-1
+// A^(M-2)MOD M = A^-1
+ll modInverseBE(ll a,ll m){
+    return binaryExponentiation(a,m-2,m);
+}
 
+//Time complexity O(log(max(A, B))).
 int main(){
-    cout<<binaryexponential(5,4,MOD);
+    ll d,x,y; // FOR EXTENDED EUCLID'S ALGORITHM
     return 0;
 }
