ef92746d7c0a090dc2c48df08fca4518080a2861
svsannidhay <sannidhayvashalsv@gmail.com>
2021-03-27 03:06:48 +0530

    some formating issues resolved

--- MATHEMATICS/Combinatorics/Stirling Numbers.cpp
+++ MATHEMATICS/Combinatorics/Stirling Numbers.cpp
@@ -96,26 +96,16 @@
 #include <bitset>
 #include <stdlib.h>
 #include <chrono>
-
+ 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#define fio                         \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);
-#define ll long long int
+#define fio  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ll  long long int
 #define ull unsigned long long int
-#define cinll(x) \
-  ll x;          \
-  cin >> x;
-#define cini(x) \
-  int x;        \
-  cin >> x;
-#define cins(x) \
-  string x;     \
-  cin >> x;
+#define cinll(x) ll x;cin >> x;
+#define cini(x) int x;cin >> x;
+#define cins(x) string x;cin >> x;
 #define vect(x) vector<ll> x
-#define vect1(x) \
-  vector<ll> x;  \
-  x.push_back(0);
+#define vect1(x) vector<ll> x;x.push_back(0);
 #define pb(x) push_back(x)
 #define mp(x, y) make_pair(x, y)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -127,87 +117,73 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 using namespace std;
 using u64 = uint64_t;
-//Safe_hashing for minimising collisions
+//Safe_hashing for minimising collisions 
 //https://codeforces.com/blog/entry/62393
-struct custom_hash
-{
-  static uint64_t splitmix64(uint64_t x)
-  {
-    // http://xorshift.di.unimi.it/splitmix64.c
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
-
-  size_t operator()(uint64_t x) const
-  {
-    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-    return splitmix64(x + FIXED_RANDOM);
-  }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
 };
+ 
+
 
 // Function to calculate the Signless Stirling numbers for first type.
-//S(n,k) : no of ways to partition n numbers into k circles
+//S(n,k) : no of ways to partition n numbers into k circles 
 
 ll dpFact[100005];
-void calFac()
-{
-  dpFact[0] = 1;
-  ll fac = 1;
-  for (ll i = 1; i <= 100000; i++)
-  {
-    fac = ((fac % MOD) * (i % MOD)) % MOD;
-    dpFact[i] = fac;
-  }
-  return;
+void calFac() {
+    dpFact[0] = 1;
+    ll fac = 1;
+    for(ll i=1;i<=100000;i++) {
+        fac = ( (fac%MOD) * (i%MOD) ) % MOD;
+        dpFact[i] = fac;
+    }
+    return;
 }
 
 ll dp[5000][5000];
 
-ll stirlingNumber(ll n, ll k)
-{
-  if (dp[n][k] != -1)
+ll stirlingNumber(ll n,ll k){
+    if(dp[n][k] != -1) return dp[n][k];
+    if (k > n) dp[n][k] = -1;
+    else if (k == 0) dp[n][k] = 0;
+    else if (k == n) dp[n][k] = 1;
+    else if (k == 1) dp[n][k] = dpFact[n-1];
+    else if (n - k == 1) dp[n][k] = (n * (n-1) / 2 ) % MOD; 
+    else dp[n][k] =  ( (stirlingNumber(n - 1, k - 1) % MOD) + ( ( ((n - 1)%MOD) * (stirlingNumber(n - 1, k)%MOD) )%MOD ) )%MOD; 
     return dp[n][k];
-  if (k > n)
-    dp[n][k] = -1;
-  else if (k == 0)
-    dp[n][k] = 0;
-  else if (k == n)
-    dp[n][k] = 1;
-  else if (k == 1)
-    dp[n][k] = dpFact[n - 1];
-  else if (n - k == 1)
-    dp[n][k] = (n * (n - 1) / 2) % MOD;
-  else
-    dp[n][k] = ((stirlingNumber(n - 1, k - 1) % MOD) + ((((n - 1) % MOD) * (stirlingNumber(n - 1, k) % MOD)) % MOD)) % MOD;
-  return dp[n][k];
 }
 
-ll solve()
-{
-  memset(dp, -1, sizeof(dp));
-  cinll(n);
-  for (ll i = 1; i <= n; i++)
-  {
-    cout << stirlingNumber(n, i) << " ";
-  }
-  return 0;
-}
-
-int main()
-{
-  fio;
-  calFac();
-  ///////////////////////////////////////////
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
-  ///////////////////////////////////////////
-  // cinll(t);
-  // for(ll i=1;i<=t;i++) {
-  solve();
-  // }
+ll solve() {
+    memset(dp,-1,sizeof(dp));
+    cinll(n);
+    for(ll i=1;i<=n;i++) {
+        cout<<stirlingNumber(n,i)<<" ";
+    }
+    return 0;
+}   
+ 
+int main(){
+    fio;
+    calFac();
+///////////////////////////////////////////    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r", stdin);
+    freopen("output.txt", "w" , stdout);
+    #endif
+///////////////////////////////////////////
+    // cinll(t);
+    // for(ll i=1;i<=t;i++) {
+        solve();
+    // }
   return 0;
 }
